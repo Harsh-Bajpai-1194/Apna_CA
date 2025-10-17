093832bb898392c14b1037c4dbfbbbf8f655ad09
Harsh Bajpai <harshbajpai1194@gmail.com>
2025-10-17 16:38:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function calculateTax() {
     document.getElementById("result").innerText = "Please enter a valid amount!";
     return;
   }
-
+ 
   if (income <= 250000) tax = 0;
   else if (income <= 500000) tax = 0.05 * (income - 250000);
   else if (income <= 1000000) tax = 12500 + 0.2 * (income - 500000);
